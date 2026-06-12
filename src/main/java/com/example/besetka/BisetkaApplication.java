b7c29f766a32079e97d041b6e85d20c4546b38ce
AbdullaOfficial <103437941+AbdullaOfficial@users.noreply.github.com>
2026-06-13 03:51:27 +0400

    small fix

--- src/main/java/com/example/besetka/BisetkaApplication.java
+++ src/main/java/com/example/besetka/BisetkaApplication.java
@@ -1,4 +1,4 @@
-package com.example.bisetka;
+package com.example.besetka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
