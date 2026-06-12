1e7571a067f3d3645b9461843ed9844e51fb71fb
Teymur Fazilov <fazilovteymur4@gmail.com>
2026-06-13 03:14:11 +0400

    collab

---
+++ src/main/java/com/example/bisetka/BisetkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bisetka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisetkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BisetkaApplication.class, args);
+    }
+
+}
