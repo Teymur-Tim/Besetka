341bcb8e04338b4fec9e7f5cc98734d9f69bc653
AbdullaOfficial <103437941+AbdullaOfficial@users.noreply.github.com>
2026-06-14 01:38:19 +0400

    small changes for name

--- src/main/java/com/example/besetka/BesetkaApplication.java
+++ src/main/java/com/example/besetka/BesetkaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BisetkaApplication {
+public class BesetkaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BisetkaApplication.class, args);
+        SpringApplication.run(BesetkaApplication.class, args);
     }
 
 }
